b6ca796c43369193c69384cbf4dc52c4ade42758
XarXsPL <adam.chabros006@gmail.com>
2019-10-08 16:50:12 +0200

    map generator class

--- DarkEra_Project/MapBC.cpp
+++ DarkEra_Project/MapBC.cpp
@@ -4,13 +4,35 @@ Mapa::Mapa(){
 	textura = new Texture();
 	sprite = new Sprite();
 	okno = MainWindow::getMainWindow()->getWindow();
+	mWys = (rand() % 30) + 10;
+	mSzer = (rand() % 30) + 10;
+	*map = new int[mSzer];
+	for (int i = 0; i < mSzer; i++){
+		map[i] = new int[mWys];
+	}
 }
 
 Mapa::~Mapa(){
 	delete textura;
 	delete sprite;
+	for (int i = 0; i < mSzer; i++)	delete[]map[i];
+	delete map;
 }
 
 void Mapa::render(){
-	okno->draw(*sprite);
+	for (int i = 0; i < mSzer; i++){
+		for (int j = 0; j < mWys; j++){
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+	//okno->draw(*sprite);
+}
+
+void Mapa::generateMap(){
+	for (int i = 0; i < mSzer; i++){
+		for (int j = 0; j < mWys; j++){
+			map[i][j] = 1;
+		}
+	}
 }
--- DarkEra_Project/main.cpp
+++ DarkEra_Project/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 int main()
 {
+	srand(time(NULL));
 	Game *gra = new Game();
 	gra->run();
 	delete gra;
